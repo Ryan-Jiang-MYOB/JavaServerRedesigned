3e80c045de943a5fa29ffec3119b626573665d24
Ryan-Jiang-MYOB <ryan.jiang@myob.com>
2018-04-04 17:09:55 +1000

    Added deployment script for AWS, please work...

--- src/main/java/JavaServerEntry.java
+++ src/main/java/JavaServerEntry.java
@@ -6,7 +6,7 @@ public class JavaServerEntry {
 
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(8080);
+            ServerSocket serverSocket = new ServerSocket(8888);
             while(true) {
                 Socket clientSocket = serverSocket.accept();
 
