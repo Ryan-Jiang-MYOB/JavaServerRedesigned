74dc406c1f5266505bf549458cfe391ee2fdbefe
Ryan-Jiang-MYOB <ryan.jiang@myob.com>
2018-04-06 13:25:01 +1000

    fixed long running script issues

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo pkill -f "java -jar"
-java -jar /home/ec2-user/JavaServer/target/JavaServer.jar 8100 > /dev/null 2> /dev/null &
+(java -jar /home/ec2-user/JavaServer/target/JavaServer.jar 8100 &> /dev/null &)
